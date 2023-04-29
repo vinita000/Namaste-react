eb08dd37aeb564ceebcf5cfbe5e5ef41e878fb7b
Vinita Kumari <vinita.kumari@qubited.com>
2023-04-29 23:33:12 +0530

    Added react code using React.createElement

---
+++ App.js
@@ -0,0 +1,43 @@
+/*
+
+<div id="parent">
+  <div id="children">
+    <h1>I am h1 tag from react</h1>
+    <h2>I am 2nd h1 tag<h2>
+  </div>
+
+  <div id="children2">
+    <h1>I am h1 tag from react</h1>
+    <h2>I am 2nd h1 tag<h2>
+  </div>
+</div>
+
+
+
+
+*/
+
+const parent = React.createElement(
+  'div', {id: 'parent'},
+  [React.createElement('div', {id: 'children'}, [
+    React.createElement('h1', {}, 'I am h1 tag from react'),
+     React.createElement('h2', {}, 'I am 2nd h1 tag')
+    ]
+  ),
+  React.createElement('div', {id: 'children2'}, [
+    React.createElement('h1', {}, 'I am h1 tag from react'),
+     React.createElement('h2', {}, 'I am 2nd h1 tag')
+    ]
+  )
+  ]
+);
+
+//JSX
+
+// const heading = React.createElement('h1', { id: 'heading' }, "Hello world React")
+
+// console.log(heading) //heading is actually is a object
+console.log(parent)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));      
+root.render(parent)
