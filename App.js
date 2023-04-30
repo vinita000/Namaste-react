2735d5c2cb58367c24fab03be6b07482768c93fb
Vinita Kumari <vinita.kumari@qubited.com>
2023-04-30 23:57:59 +0530

    episode 2-we are creating jsx

--- App.js
+++ App.js
@@ -1,46 +1,14 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-/*
 
-<div id="parent">
-  <div id="children">
-    <h1>I am h1 tag from react</h1>
-    <h2>I am 2nd h1 tag<h2>
-  </div>
-
-  <div id="children2">
-    <h1>I am h1 tag from react</h1>
-    <h2>I am 2nd h1 tag<h2>
-  </div>
-</div>
-
-
-
-
-*/
-
-const parent = React.createElement(
-  'div', {id: 'parent'},
-  [React.createElement('div', {id: 'children'}, [
-    React.createElement('h1', {}, 'I am h1 tag from react'),
-     React.createElement('h2', {}, 'I am 2nd h1 tag3333')
-    ]
-  ),
-  React.createElement('div', {id: 'children2'}, [
-    React.createElement('h1', {}, 'I am h1 tag111 from react111'),
-     React.createElement('h2', {}, 'I am 2nd h1 tag12222')
-    ]
-  )
-  ]
-);
-
+const heading = React.createElement('h1', { id: 'heading' }, "Hello world React")
+console.log(heading);
 //JSX
 
-// const heading = React.createElement('h1', { id: 'heading' }, "Hello world React")
-
+const jsxheading = <h1 id="heading">Hello world React from jsx</h1>;
 // console.log(heading) //heading is actually is a object
-console.log(parent)
+console.log(jsxheading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));      
-root.render(parent)
+root.render(jsxheading)
