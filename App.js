82588ce5c1f3b7bee01d5bd0f68e725d6df975e5
Vinita Kumari <vinita.kumari@qubited.com>
2023-05-01 14:39:51 +0530

    episode 3-component composition

--- App.js
+++ App.js
@@ -19,5 +19,45 @@ const pqr = (
   </h1>
 ); ///if it is multi liner then wrap with braces as bibel understand where jsx start and where end
 
+///javascript function
+const fn = () => {
+  return true
+}
+//or
+
+const fn1 = () => true;
+
+///both defining function in javascript are same.
+
+/// functional component
+/// It is just a javascript function which returns some jsx.
+const HeadingComponent = () => {
+  return (<h1>Hello I am functional componet</h1>);
+}
+
+//or
+const HeadingComponent1 = () => <h1>Hello I am functional componet</h1>;
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));      
-root.render(jsxheading)
+// root.render(jsxheading)
+
+
+// one component inside other component
+const Title = () => (
+  <div id='title'>This is title component</div>
+)
+
+const BodyComponent = () => {
+  return(
+   
+    <div id="body"> 
+    <h1>This is body component</h1>
+    <Title />
+    </div>
+  )
+};
+
+///^ this is component composition
+///Babel will do transpiling and react render html by  result of root.render
+root.render(<BodyComponent />); //Babel uderstand some component exixts
