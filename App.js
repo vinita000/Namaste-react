30c81d06c3df4d53fe95c708bb2f698a92dda0ef
Vinita Kumari <vinita.kumari@qubited.com>
2023-05-01 10:46:48 +0530

    episode 2-knowing about JSX

--- App.js
+++ App.js
@@ -8,9 +8,16 @@ console.log(heading);
 //Jsx(transpiled before it reaches the JS Engine) - Parcel - Babel
 
 //JSX => Babel is converted to React.CreateElement JS Object => HTMLElement
-const jsxheading = <h1 id="heading">Hello world React from jsx</h1>;
+const jsxheading = <h1 id="heading" className='heading' tabIndex='10'>Hello world React from jsx</h1>; ///if it one liner then ok
 // console.log(heading) //heading is actually is a object
 console.log(jsxheading);
+
+
+const pqr = (
+  <h1 id="heading" className='heading' tabIndex='10'>
+    Hello world React from jsx
+  </h1>
+); ///if it is multi liner then wrap with braces as bibel understand where jsx start and where end
 
 const root = ReactDOM.createRoot(document.getElementById("root"));      
 root.render(jsxheading)
